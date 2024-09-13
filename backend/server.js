3dddbe86cf74c044e8f3d034d192907a7dd58219
karanagg166 <22bcs130.@iiitdmj.ac.in>
2024-09-13 17:38:08 +0530

    ho

--- backend/server.js
+++ backend/server.js
@@ -40,11 +40,6 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
 	connectDB();
